63c5558117b17a83f651a7322a7ac28149c13c34
Ethan Campbell <ethancampbell997@users.noreply.github.com>
2024-07-09 23:21:30 -0400

    Add rows to grid

--- javascript.js
+++ javascript.js
@@ -8,7 +8,12 @@ for(let i = 0; i < 16; i++){
     //    content.appendChild(subcontent);
     //}
     container.appendChild(content);
+    for(let j =0; j < 16; j++){
+        const row = document.createElement("div");
+        row.classList.add("row");
+        content.appendChild(row);
+    }
 }
-const something = document.createElement("div");
-something.textContent = "hi";
-container.appendChild(something);
+//const something = document.createElement("div");
+//something.textContent = "hi";
+//container.appendChild(something);
