52d952e77be9c4d8cbab21b000fa37c109ddc955
Ethan Campbell <ethancampbell997@users.noreply.github.com>
2024-07-09 23:37:18 -0400

    Add color change when hovering over squares

--- javascript.js
+++ javascript.js
@@ -1,19 +1,16 @@
 const container = document.querySelector("#container");
 for(let i = 0; i < 16; i++){
     const content = document.createElement("div");
-    //content.classList.add("row");
-    //container.appendChild(content);
-    //for(let j =0; j < 16; j++){
-    //    const subcontent = document.createElement("div");
-    //    content.appendChild(subcontent);
-    //}
     container.appendChild(content);
     for(let j =0; j < 16; j++){
         const row = document.createElement("div");
         row.classList.add("row");
-        content.appendChild(row);
+        content.appendChild(row);       
     }
 }
-//const something = document.createElement("div");
-//something.textContent = "hi";
-//container.appendChild(something);
+const rows = document.querySelectorAll(".row");
+rows.forEach(element => {
+    element.addEventListener("mouseover", () => {
+        element.classList.add("hovered");
+    })
+});
